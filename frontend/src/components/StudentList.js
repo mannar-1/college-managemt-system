ad5b403507e827f5253cc7d25363aaa872e6938b
mannar-1 <malyada11@gmail.com>
2023-07-15 11:45:47 +0530

    serach search stylling

--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -1,85 +1,4 @@
-// import React, { useEffect, useState } from 'react';
 
-// const StudentList = ({ searchResult }) => {
-//   const [students, setStudents] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const response = await fetch('/college');
-//       if (response.ok) {
-//         const data = await response.json();
-//         setStudents(data);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const groupStudentsByCollege = () => {
-//     const colleges = {};
-
-//     students.forEach((student) => {
-//       const college = student.collegeId;
-
-//       if (!colleges[college]) {
-//         colleges[college] = [];
-//       }
-
-//       colleges[college].push(student);
-//     });
-
-//     return colleges;
-//   };
-
-//   const renderStudentDetails = (student) => {
-//     return (
-//       <div key={student._id} style={{ backgroundColor: 'lightblue', marginBottom: '10px' }}>
-//         <h3>{student.name}</h3>
-//         <p>Roll Number: {student.rollno}</p>
-//         <p>College: {student.collegeId}</p>
-//         <p>Math Marks: {student.mathmarks}</p>
-//         <p>Physics Marks: {student.phymarks}</p>
-//         <p>Chemistry Marks: {student.chemmarks}</p>
-//       </div>
-//     );
-//   };
-
-//   return (
-//     <div>
-//       {Object.entries(groupStudentsByCollege()).map(([college, students]) => (
-//         <div key={college} style={{ border: '1px solid black', padding: '10px', marginBottom: '10px' }}>
-//           <h2>{college}</h2>
-//           <ul>
-//             {students.map((student) => (
-//               <li key={student._id}>{student.name}</li>
-//             ))}
-//           </ul>
-//         </div>
-//       ))}
-
-//       {searchResult && (
-//         <div
-//           style={{
-//             position: 'fixed',
-//             top: '50%',
-//             left: '50%',
-//             transform: 'translate(-50%, -50%)',
-//             background: 'black',
-//             color: 'white',
-//             padding: '20px',
-//             zIndex: 9999,
-//           }}
-//         >
-//           Search Result:
-//           {searchResult.map((student) => renderStudentDetails(student))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default StudentList;
-// StudentList.js
 import React, { useEffect, useState } from 'react';
 import '../styles.css'; // Import the CSS file
 import img from '../assets/pic.jpg';
@@ -167,68 +86,29 @@ const StudentList = () => {
         }} value={searchQuery} onChange={handleSearchQueryChange} placeholder='Search here' pattern='10px' /></center>
         <p></p>
         <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-          <button style={{
-            backgroundColor: 'black',
-            color: 'white',
-            width: '200px',
-            height: '25px',
-            borderRadius: '10px'
-          }} onClick={searchByRollNo} >Search by Roll No</button>
-          <button style={{
-            backgroundColor: 'black',
-            color: 'white',
-            width: '200px',
-            height: '25px',
-            borderRadius: '10px'
-          }} onClick={searchByName}>Search by Name</button>
-          <button style={{
-            backgroundColor: 'black',
-            color: 'white',
-            width: '200px',
-            height: '25px',
-            borderRadius: '10px'
-          }} onClick={searchByBranch}>Search by Branch</button>
-          <button style={{
-            backgroundColor: 'black',
-            color: 'white',
-            width: '200px',
-            height: '25px',
-            borderRadius: '10px'
-          }} onClick={searchByCollege}>Search by College</button>
-          <button style={{
-            backgroundColor: 'black',
-            color: 'white',
-            width: '200px',
-            height: '25px',
-            borderRadius: '10px'
-          }} onClick={() => setSearchResult(null)}>Clear Search</button>
+          <button 
+             className="search-button"
+          onClick={searchByRollNo} >Search by Roll No</button>
+          <button
+            className="search-button"
+          onClick={searchByName}>Search by Name</button>
+          <button 
+            className="search-button"
+          onClick={searchByBranch}>Search by Branch</button>
+          <button 
+           
+           className="search-button"
+           onClick={searchByCollege}>Search by College</button>
+          <button 
+            className="search-button"
+          onClick={() => setSearchResult(null)}>Clear Search</button>
 
         </div>
         <p></p>
         <p></p>
       </div>
 
-      {/* {searchResult && (
-        <div className="popup">
-          <div className="popup-content">
-            <h2>Search Result</h2>
-            <ul>
-              {searchResult.map((result) => (
-                <>
-                  <li key={result._id}>{result.name}</li>
-                  <li>{result.rollno}</li>
-                  <li>{result.branch}</li>
-                  <li>{result.collegeId}</li>
-                  <li>{result.mathmarks + result.chemmarks + result.phymarks}</li>
-                </>
-              ))}
-            </ul>
-            
-              X
-            </button>
-          </div>
-        </div>
-      )} */}
+      
 
       {searchResult && (
         <div>
